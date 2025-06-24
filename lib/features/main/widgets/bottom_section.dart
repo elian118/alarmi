2acec4cef925e73ae15270a5c87feae5d4dec1fc
김광회 <elian118@naver.com>
2025-06-24 18:49:56 +0900

    낚시하기 버튼 연결

--- lib/features/main/widgets/bottom_section.dart
+++ lib/features/main/widgets/bottom_section.dart
@@ -1,5 +1,7 @@
 import 'package:alarmi/common/consts/gaps.dart';
+import 'package:alarmi/features/main/widgets/feed_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/svg.dart';
 
 class BottomSection extends StatefulWidget {
@@ -10,10 +12,126 @@ class BottomSection extends StatefulWidget {
 }
 
 class _BottomSectionState extends State<BottomSection> {
+  bool _isOpenCatMenus = false;
+  bool _isOpenGenAlarms = false;
+  bool _isLight = false;
+  String _message = '좋은 아침!';
+
+  void _toggleLight() {
+    setState(() {
+      _isLight = !_isLight;
+    });
+  }
+
+  void _setMessage(newMsg) {
+    setState(() {
+      _message = newMsg;
+    });
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        _message = '좋은 아침!';
+      });
+    });
+  }
+
+  void toggleCatMenus() {
+    setState(() {
+      _isOpenCatMenus = !_isOpenCatMenus;
+    });
+  }
+
+  void toggleGenAlarms() {
+    setState(() {
+      _isOpenGenAlarms = !_isOpenGenAlarms;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 3),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                spacing: 12,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        '낚시하기',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Gaps.h12,
+                      IconButton(
+                        onPressed: () {
+                          toggleCatMenus();
+                        },
+                        // 복잡해서 그런지, svg 형식 인식 못함 -> png로 변경
+                        icon: Image.asset(
+                          "assets/images/fishing_rod_icon.png",
+                          width: 18,
+                          height: 18,
+                          fit: BoxFit.contain,
+                        ),
+                        style: IconButton.styleFrom(
+                          padding: EdgeInsets.all(13),
+                          backgroundColor: Colors.white,
+                          shape: CircleBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        '고양이 밥 주기',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Gaps.h12,
+                      IconButton(
+                        onPressed: () {
+                          toggleCatMenus();
+                          showModalBottomSheet(
+                            context: context,
+                            builder:
+                                (context) => FeedDialog(
+                                  toggleLight: _toggleLight,
+                                  setMessage: _setMessage,
+                                ),
+                            isScrollControlled: true,
+                          );
+                        },
+                        icon: SvgPicture.asset(
+                          "assets/images/feed.svg",
+                          width: 18,
+                          height: 18,
+                          fit: BoxFit.contain,
+                        ),
+                        style: IconButton.styleFrom(
+                          padding: EdgeInsets.all(13),
+                          backgroundColor: Colors.white,
+                          shape: CircleBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+            .animate(target: _isOpenCatMenus ? 1 : 0)
+            .slideY(begin: -0.5, end: 0.0)
+            .fadeIn(),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           spacing: 14,
@@ -80,14 +198,32 @@ class _BottomSectionState extends State<BottomSection> {
               ),
             ),
             Spacer(),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.close),
-              style: IconButton.styleFrom(
-                backgroundColor: Colors.white,
-                shape: CircleBorder(),
-              ),
-            ),
+            _isOpenCatMenus
+                ? IconButton(
+                  onPressed: toggleCatMenus,
+                  icon: Icon(Icons.close, size: 30),
+                  style: IconButton.styleFrom(backgroundColor: Colors.white),
+                )
+                : IconButton(
+                  onPressed: toggleCatMenus,
+                  icon: SvgPicture.asset(
+                    _isOpenCatMenus
+                        ? "assets/images/close.svg"
+                        : "assets/images/cat_icon.svg",
+                    width: 18,
+                    height: 18,
+                    fit: BoxFit.contain,
+                    colorFilter:
+                        _isOpenCatMenus
+                            ? null
+                            : ColorFilter.mode(Colors.black, BlendMode.srcIn),
+                  ),
+                  style: IconButton.styleFrom(
+                    padding: EdgeInsets.all(13),
+                    backgroundColor: Colors.white,
+                    shape: CircleBorder(),
+                  ),
+                ),
           ],
         ),
       ],
