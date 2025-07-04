94418a5fea617ed74fce03d2320ceefeea532886
김광회 <elian118@naver.com>
2025-07-04 18:12:46 +0900

    속성 변경

--- lib/common/consts/raw_data/weekdays.dart
+++ lib/common/consts/raw_data/weekdays.dart
@@ -1,11 +1,11 @@
 import 'package:alarmi/features/alarm/models/weekday.dart';
 
 List<Weekday> weekdays = [
-  Weekday(name: '일', isSelected: false, dateTime: DateTime.sunday),
-  Weekday(name: '월', isSelected: false, dateTime: DateTime.monday),
-  Weekday(name: '화', isSelected: false, dateTime: DateTime.tuesday),
-  Weekday(name: '수', isSelected: false, dateTime: DateTime.wednesday),
-  Weekday(name: '목', isSelected: false, dateTime: DateTime.thursday),
-  Weekday(name: '금', isSelected: false, dateTime: DateTime.friday),
-  Weekday(name: '토', isSelected: false, dateTime: DateTime.saturday),
+  Weekday(name: '일', isSelected: false, id: DateTime.sunday),
+  Weekday(name: '월', isSelected: false, id: DateTime.monday),
+  Weekday(name: '화', isSelected: false, id: DateTime.tuesday),
+  Weekday(name: '수', isSelected: false, id: DateTime.wednesday),
+  Weekday(name: '목', isSelected: false, id: DateTime.thursday),
+  Weekday(name: '금', isSelected: false, id: DateTime.friday),
+  Weekday(name: '토', isSelected: false, id: DateTime.saturday),
 ];
--- lib/features/alarm/models/weekday.dart
+++ lib/features/alarm/models/weekday.dart
@@ -1,18 +1,14 @@
 class Weekday {
+  final int id;
   final String name;
-  final int dateTime;
   bool isSelected;
 
-  Weekday({
-    required this.name,
-    required this.dateTime,
-    required this.isSelected,
-  });
+  Weekday({required this.id, required this.name, required this.isSelected});
 
   Weekday copyWith({String? name, bool? isSelected}) {
     return Weekday(
       name: name ?? this.name,
-      dateTime: dateTime,
+      id: id,
       isSelected: isSelected ?? this.isSelected,
     );
   }
