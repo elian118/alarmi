2ce631bcc4692a070abff5504139dd328ff90cac
김광회 <elian118@naver.com>
2025-07-04 15:41:59 +0900

    불필요 코드 삭제

--- lib/features/test/screens/alarm_test_screen.dart
+++ lib/features/test/screens/alarm_test_screen.dart
@@ -15,44 +15,6 @@ class AlarmTestScreen extends StatelessWidget {
 
   const AlarmTestScreen({super.key});
 
-  // void onTabAlarmSet(BuildContext context, int delay) async {
-  //   // 현재 시간 + 5초 후에 알람 설정
-  //   final now = DateTime.now();
-  //   final dateTime = DateTime(
-  //     now.year,
-  //     now.month,
-  //     now.day,
-  //     now.hour,
-  //     now.minute,
-  //     now.second + delay, // 5초 후
-  //   );
-  //
-  //   await Alarm.set(
-  //     alarmSettings: setAlarmSettings(
-  //       dateTime,
-  //       0.8,
-  //       'assets/audios/default/test.mp3',
-  //     ),
-  //   );
-  //   ScaffoldMessenger.of(
-  //     context,
-  //   ).showSnackBar(SnackBar(content: Text('알람 설정됨: ${dateTime.toLocal()}')));
-  // }
-  //
-  // void onTabStopAlarm(BuildContext context) async {
-  //   await Alarm.stop(42); // ID로 알람 중지
-  //   ScaffoldMessenger.of(
-  //     context,
-  //   ).showSnackBar(const SnackBar(content: Text('알람 중지됨')));
-  // }
-  //
-  // void onTabGetAlarmCounts(BuildContext context) async {
-  //   final alarms = await Alarm.getAlarms();
-  //   ScaffoldMessenger.of(
-  //     context,
-  //   ).showSnackBar(SnackBar(content: Text('현재 설정된 알람 수: ${alarms.length}')));
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,23 +23,6 @@ class AlarmTestScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // ElevatedButton(
-            //   onPressed: () => onTabAlarmSet(context, delay),
-            //   child: const Text('알람 설정 ($delay)'),
-            // ),
-            // const SizedBox(height: 20),
-            // ElevatedButton(
-            //   onPressed: () => onTabStopAlarm(context),
-            //   child: const Text('알람 중지'),
-            // ),
-            // const SizedBox(height: 20),
-            // ElevatedButton(
-            //   onPressed: () => onTabGetAlarmCounts(context),
-            //   child: const Text('설정된 알람 확인'),
-            // ),
-            // Gaps.v24,
-            // CstDivider(width: 100, thickness: 10),
-            // Gaps.v24,
             ElevatedButton(
               onPressed: () async {
                 Random random = Random();
