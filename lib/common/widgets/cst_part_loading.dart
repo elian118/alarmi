a929c45c0375101fc55009ae5eec8552d2ad8157
김광회 <elian118@naver.com>
2025-07-23 12:29:03 +0900

    공통화

---
+++ lib/common/widgets/cst_part_loading.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CstPartLoading extends StatelessWidget {
+  const CstPartLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
